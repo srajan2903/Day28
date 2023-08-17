8d5cacba4f906e949d369262dd6702ca776c9de4
Your Name <you@example.com>
2023-08-17 16:59:00 +0530

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "This is my default action..";
+        //}
+        public string Welcome(string name, int id=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
